21515e3effa86c6c7a93eaa25444173465cac9b4
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2022-12-04 20:10:23 +0100

    Day 4, part 2

---
+++ Day04/puzzle2.php
@@ -0,0 +1,29 @@
+<?php
+
+function hasPartialOverlap(...$ranges) {
+  usort($ranges, function ($rangeA, $rangeB) {
+    $cmp = $rangeA[0] - $rangeB[0];
+
+    return $cmp === 0 ? $rangeB[1] - $rangeA[1] : $cmp;
+  });
+
+  return $ranges[0][0] <= $ranges[1][1] && $ranges[0][1] >= $ranges[1][0];
+
+}
+
+$handle = fopen('input.txt', 'r');
+$overlaps = 0;
+while (($line = fgets($handle)) !== false) {
+  $line = trim($line);
+  $matches = [];
+  preg_match_all('/^(\d+)-(\d+),(\d+)-(\d+)$/', $line, $matches);
+  $ranges = [
+    [intval($matches[1][0]), intval($matches[2][0])],
+    [intval($matches[3][0]), intval($matches[4][0])],
+  ];
+  if (hasPartialOverlap(...$ranges)) {
+    $overlaps++;
+  }
+}
+
+print $overlaps . "\n";
