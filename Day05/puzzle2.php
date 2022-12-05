7bc861fc156d1f7bc0601cf37a946f46b08deca7
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2022-12-05 11:57:40 +0100

    Day 5, part 2
    Yups, definitely could've used arrays, but still, fancy++.

---
+++ Day05/puzzle2.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @return \SplStack[]
+ */
+function parseStacks(array $stackLines, $stackAmount) {
+  $stacks = [];
+  for ($i = 0; $i < $stackAmount; $i++) {
+    $stack = new SplStack();
+    $stacks[] = $stack;
+  }
+  // SplStack is LIFO, but we're reading top to bottom, so reverse the lines.
+  foreach (array_reverse($stackLines) as $stackLine) {
+    $crates = array_map('trim', str_split($stackLine, 4));
+    foreach ($crates as $stackNumber => $crate) {
+      if ($crate) {
+        $stacks[$stackNumber]->push($crate);
+      }
+    }
+  }
+
+  return $stacks;
+}
+
+function parseMove($line) {
+  $matches = [];
+  preg_match('/move\s+(\d+)\s+from\s+(\d+)\s+to\s+(\d+)/', $line, $matches);
+  array_shift($matches);
+  return $matches;
+}
+
+$handle = fopen('input.txt', 'r');
+$stackLines = [];
+$stackAmount = 0;
+while (($line = fgets($handle)) !== false) {
+  if (!trim($line)) {
+    // Parsed all stacks, last line is stack numbers.
+    $stackAmount = preg_match_all('/\s\d+/', array_pop($stackLines));
+    break;
+  }
+  $stackLines[] = $line;
+}
+
+$stacks = parseStacks($stackLines, $stackAmount);
+
+while (($line = fgets($handle)) !== false) {
+  if (!trim($line)) {
+    continue;
+  }
+  list($amount, $source, $dest) = parseMove($line);
+  $cratesToMove = new SplStack();
+  for ($i = 0; $i < $amount; $i++) {
+    $cratesToMove->push($stacks[$source-1]->pop());
+  }
+  for ($j = 0; $j < $amount; $j++) {
+    $stacks[$dest-1]->push($cratesToMove->pop());
+  }
+}
+
+print 'Top crates: ';
+foreach ($stacks as $stack) {
+  print str_replace(['[', ']'], '', $stack->top());
+}
+print PHP_EOL;
