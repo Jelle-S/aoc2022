a86546a280f545874700259444894d53ffd3d55a
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2022-12-06 08:59:14 +0100

    Day 6, part 2

---
+++ Day06/puzzle2.php
@@ -0,0 +1,17 @@
+<?php
+$buffer = str_split(file_get_contents('input.txt'));
+$marker_length = 14;
+$marker = [];
+$index = 0;
+while(count($buffer)) {
+  $index++;
+  $marker[] = array_shift($buffer);
+  if (count($marker) > $marker_length) {
+    array_shift($marker);
+  }
+  if (count($marker) === $marker_length && count(array_unique($marker)) === $marker_length) {
+    break;
+  }
+}
+
+echo $index;
